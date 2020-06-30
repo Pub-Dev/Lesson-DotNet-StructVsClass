623da1c197d3236114efb0b9d06fbff3f640f8f5
Humberto Pereira <humberto_henrique1@live.com>
2020-06-29 22:47:19 -0300

    adicionando projeto inicial com exemplos da utilizacao de classes

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace StructVsClass
+{
+    public class Pessoa
+    {
+        public int Idade { get; set; }
+        public string Nome { get; set; }
+    }
+
+    public class Emprego
+    {
+        public string Nome { get; set; }
+    }
+
+    public struct Cliente
+    {
+        public string Nome { get; set; }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Exemplo 1
+            //Pessoa pessoa;
+
+            //pessoa = new Pessoa();
+            //pessoa.Nome = "Pub-Dev";
+            //pessoa.Idade = 1;
+
+            //Exemplo 2
+            //Pessoa pessoa;
+            //Pessoa pessoa2;
+
+            //pessoa = new Pessoa();
+            //pessoa.Nome = "Pub-Dev";
+            //pessoa.Idade = 1;
+
+            //pessoa2 = pessoa;
+
+            //pessoa2.Nome = "Dev-Pub";
+
+            //Console.WriteLine(pessoa.Nome);
+            //Console.ReadKey();
+
+            //Exemplo 3
+            //Pessoa pessoa;
+
+            //pessoa = new Pessoa();
+            //pessoa.Nome = "Pub-Dev";
+            //pessoa.Idade = 1;
+
+            //pessoa2 = pessoa;
+
+            //MudarNome(pessoa);
+
+            //Console.WriteLine(pessoa.Nome);
+            //Console.ReadKey();
+        }
+
+        private static void MudarNome(Pessoa pessoa)
+        {
+            pessoa.Nome = "Novo Nome";
+        }
+    }
+}
