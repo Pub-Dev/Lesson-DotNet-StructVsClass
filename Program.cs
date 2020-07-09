fbc5631042916300bd5d5bdf2be3c553d52d2b3f
Humberto Pereira <humberto_henrique1@live.com>
2020-07-09 19:56:06 -0300

    adicionando exemplo com a stack

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace StructVsClass
+﻿using System;
+
+namespace StructVsClass
 {
     public class Pessoa
     {
@@ -18,48 +20,77 @@
 
     internal class Program
     {
+        //Exemplo 1 Stack
         private static void Main(string[] args)
         {
-            //Exemplo 1
-            //Pessoa pessoa;
+            int primeiro = 10;
+            int segundo = 5;
 
-            //pessoa = new Pessoa();
-            //pessoa.Nome = "Pub-Dev";
-            //pessoa.Idade = 1;
+            int terceiro = primeiro + segundo;
 
-            //Exemplo 2
-            //Pessoa pessoa;
-            //Pessoa pessoa2;
-
-            //pessoa = new Pessoa();
-            //pessoa.Nome = "Pub-Dev";
-            //pessoa.Idade = 1;
-
-            //pessoa2 = pessoa;
-
-            //pessoa2.Nome = "Dev-Pub";
-
-            //Console.WriteLine(pessoa.Nome);
-            //Console.ReadKey();
-
-            //Exemplo 3
-            //Pessoa pessoa;
-
-            //pessoa = new Pessoa();
-            //pessoa.Nome = "Pub-Dev";
-            //pessoa.Idade = 1;
-
-            //pessoa2 = pessoa;
-
-            //MudarNome(pessoa);
-
-            //Console.WriteLine(pessoa.Nome);
-            //Console.ReadKey();
+            return;
         }
 
-        private static void MudarNome(Pessoa pessoa)
-        {
-            pessoa.Nome = "Novo Nome";
-        }
+        //Exemplo 2 Stack
+        //private static void Main(string[] args)
+        //{
+        //    int primeiro = 10;
+        //    int segundo = 5;
+
+        //    int terceiro = Somar(primeiro, segundo);
+
+        //    return;
+        //}
+
+        //private static int Somar(int numero1, int numero2)
+        //{
+        //    int soma = numero1 + numero2;
+
+        //    return soma;
+        //}
+
+        //private static void Main(string[] args)
+        //{
+        //Exemplo 1
+        //Pessoa pessoa;
+
+        //pessoa = new Pessoa();
+        //pessoa.Nome = "Pub-Dev";
+        //pessoa.Idade = 1;
+
+        //Exemplo 2
+        //Pessoa pessoa;
+        //Pessoa pessoa2;
+
+        //pessoa = new Pessoa();
+        //pessoa.Nome = "Pub-Dev";
+        //pessoa.Idade = 1;
+
+        //pessoa2 = pessoa;
+
+        //pessoa2.Nome = "Dev-Pub";
+
+        //Console.WriteLine(pessoa.Nome);
+        //Console.ReadKey();
+
+        //Exemplo 3
+        //Pessoa pessoa;
+
+        //pessoa = new Pessoa();
+        //pessoa.Nome = "Pub-Dev";
+        //pessoa.Idade = 1;
+
+        //pessoa2 = pessoa;
+
+        //MudarNome(pessoa);
+
+        //Console.WriteLine(pessoa.Nome);
+        //Console.ReadKey();
+        //}
+
+        //private static void MudarNome(Pessoa pessoa)
+        //{
+        //    pessoa.Nome = "Novo Nome";
+        //}
     }
 }
